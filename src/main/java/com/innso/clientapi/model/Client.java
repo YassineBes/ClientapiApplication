edd68caf34aa59b321381c60ca92e08849e3142f
Yassine Besbes <yassinebesbes@air-de-yassine.home>
2021-05-30 22:54:44 +0200

    first commit

---
+++ src/main/java/com/innso/clientapi/model/Client.java
@@ -0,0 +1,9 @@
+package com.innso.clientapi.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.lang.Inherited;
+
+@EntityScan
+public class Client {
+}
---
+++ src/main/java/com/innso/clientapi/model/Message.java
@@ -0,0 +1,7 @@
+package com.innso.clientapi.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+public class Message {
+}
